13ce701542a92a94093f8b8b85f51609e6c3ded6
keta4 <82580781+keta4@users.noreply.github.com>
2022-12-09 11:29:34 +0900

    create list.html and UserController.java

---
+++ web-app/src/main/java/com/example/web/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.example.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+
+    @GetMapping(value = "/user/list")
+    public String displayList(Model model) {
+        model.addAttribute("screenName", "ユーザー情報: 一覧画面");
+        return "user/list";
+    }
+}
