b7c910a7d5eeb069e3f33266285d8a6bfe29e15f
keta4 <82580781+keta4@users.noreply.github.com>
2022-12-11 04:17:36 +0900

    feature:'GET /user/list'

--- web-app/src/main/java/com/example/web/controller/UserController.java
+++ web-app/src/main/java/com/example/web/controller/UserController.java
@@ -1,16 +1,31 @@
 package com.example.web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.example.web.service.UserService;
+
 @Controller
 public class UserController {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/user/list")
     public String displayList(Model model) {
-        model.addAttribute("screenName", "ユーザー情報: 一覧画面");
+        var userlist = userService.searchAll();
+        System.out.println(userlist);
+        model.addAttribute("userlist", userlist);
         return "user/list";
     }
+
+    // @GetMapping("/user/{id}")
+    // public String displayDetails(@PathVariable Long id, Model model) {
+    // var foundUser = userService.findById(id);
+    // model.addAttribute("userData", foundUser);
+    // return "/user/details";
+    // }
 
 }
