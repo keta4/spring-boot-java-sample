812f599d466ba5db6ba665c48a3bb0cb28d604d6
keta4 <82580781+keta4@users.noreply.github.com>
2022-12-11 21:02:20 +0900

    feature: /user/list and /user/details

--- web-app/src/main/java/com/example/web/service/UserService.java
+++ web-app/src/main/java/com/example/web/service/UserService.java
@@ -1,47 +1,25 @@
 package com.example.web.service;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.web.entity.User;
+import com.example.web.repository.UserRepository;
 
 @Service
 public class UserService {
 
-    private ArrayList<User> supplyUserList() {
-        var userlist = new ArrayList<User>();
+    @Autowired
+    private UserRepository userRepository;
 
-        var phone = "090-1234-1234";
-        var address = "東京都渋谷区1-1";
-        var updateDate = LocalDateTime.of(2022, 12, 2, 0, 0, 0);
-        var createDate = LocalDateTime.of(2022, 12, 1, 0, 0, 0);
-
-        var Alice = new User(1L, "Alice", address, phone, updateDate, createDate);
-        var Bob = new User(2L, "Bob", address, phone, updateDate, createDate);
-        var Carol = new User(3L, "Carol", address, phone, updateDate, createDate);
-
-        userlist.add(Alice);
-        userlist.add(Bob);
-        userlist.add(Carol);
-
-        return userlist;
-    }
-
-    public ArrayList<User> searchAll() {
-        return supplyUserList();
+    public List<User> searchAll() {
+        return userRepository.findAll();
     }
 
     public User findById(Long id) {
-        var userlist = supplyUserList();
-
-        var foundUser = userlist.stream()
-                .filter(user -> user.getId().equals(id))
-                .findFirst()
-                .get();
-
-        return foundUser;
+        return userRepository.findById(id).get();
     }
 
 }
