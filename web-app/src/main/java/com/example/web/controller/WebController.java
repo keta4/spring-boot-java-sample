fbe2d599b181f70df7f0ff2afe45cf3c0c52c32a
keta4 <82580781+keta4@users.noreply.github.com>
2022-12-11 19:44:33 +0900

    template files devided

--- web-app/src/main/java/com/example/web/controller/WebController.java
+++ web-app/src/main/java/com/example/web/controller/WebController.java
@@ -1,15 +1,13 @@
 package com.example.web.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WebController {
 
     @GetMapping("/")
-    public String index(Model model) {
-        model.addAttribute("message", "web-app");
+    public String index() {
         return "index";
     }
 
