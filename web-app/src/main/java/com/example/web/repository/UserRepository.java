f19ab55bb01b30ac5ab91411250c732d82bf0113
keta4 <82580781+keta4@users.noreply.github.com>
2022-12-16 04:04:29 +0900

    updated

--- web-app/src/main/java/com/example/web/repository/UserRepository.java
+++ web-app/src/main/java/com/example/web/repository/UserRepository.java
@@ -1,10 +1,8 @@
 package com.example.web.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.example.web.entity.User;
 
-@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
