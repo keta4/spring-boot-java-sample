d00df46a8c179c55c652d1671700a3761c8534f3
keta4 <82580781+keta4@users.noreply.github.com>
2022-12-19 18:52:22 +0900

    explicit type

--- web-app/src/main/java/com/example/web/controller/UserController.java
+++ web-app/src/main/java/com/example/web/controller/UserController.java
@@ -1,11 +1,14 @@
 package com.example.web.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import com.example.web.entity.User;
 import com.example.web.service.UserService;
 
 @Controller
@@ -16,14 +19,14 @@ public class UserController {
 
     @GetMapping("/user/list")
     public String displayList(Model model) {
-        var userlist = userService.searchAll();
+        List<User> userlist = userService.searchAll();
         model.addAttribute("userlist", userlist);
         return "user/list";
     }
 
     @GetMapping("/user/{id}")
     public String displayDetails(@PathVariable Long id, Model model) {
-        var user = userService.findById(id);
+        User user = userService.findById(id);
         model.addAttribute("userData", user);
         return "/user/details";
     }
