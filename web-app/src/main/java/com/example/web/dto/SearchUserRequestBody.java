6148af08797382ab6db1235eabedfb35903736ae
keta4 <82580781+keta4@users.noreply.github.com>
2022-12-22 03:42:35 +0900

    small fixes

--- web-app/src/main/java/com/example/web/dto/SearchUserRequestBody.java
+++ web-app/src/main/java/com/example/web/dto/SearchUserRequestBody.java
@@ -11,8 +11,6 @@ public class SearchUserRequestBody {
     /**
      * 名前
      */
-    // @NotEmpty(message = "名前を入力してください")
-    // @Size(max = 100, message = "名前は100桁以内で入力してください")
     private String name;
 
 }
